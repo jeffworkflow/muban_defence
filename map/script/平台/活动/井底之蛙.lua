d53750ddb3b2afa3c61d594e83e3d7cf1268eeff
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2019-08-11 02:04:36 +0800

    8-11-1

--- map/script/平台/活动/井底之蛙.lua
+++ map/script/平台/活动/井底之蛙.lua
@@ -4,11 +4,11 @@ mt{
 --等久
 level = 1,
 --图标
-art = [[xique.blp]],
+art = [[jdzw.blp]],
 --说明
 tip = [[ 
-|cffffe799【活动时间】|r|cff00ff008月6日-8月8日
-|cffffe799【活动说明】|r|cff00ff00金风玉露一相逢，便胜却人间无数。年年岁岁架鹊桥，牛郎织女偷相会。少侠既然也是有心之人，还请帮忙收集|cffffff00喜鹊翎毛|r|cffcccccc（挖宝掉落）
+|cffffe799【活动时间】|r|cff00ff008月11日-8月14日
+|cffffe799【活动说明】|r|cff00ff00雨过不知龙去处，一池草色万蛙鸣。|cff00ffff这几天刮台风，基地经常出现一些青蛙，天天在那里哇哇叫。|cff00ff00还请帮忙|cffff0000把青蛙抓起来|r|cff00ff00，放到|cffffff00右边花园的井里|r
 ]],
 --物品类型
 item_type = '神符',
@@ -33,13 +33,14 @@ mt{
 --等久
 level = 1,
 --图标
-art = [[lushui.blp]],
+art = [[bsdqw.blp]],
 --说明
 tip = [[
 
-|cff00ff00传说七夕节时的露水是牛郎织女相会时的眼泪,如抹在眼上和手上,可使人眼明手快。|cffffff00使用后增加10%攻速|r
 
-|cffcccccc七夕活动物品|r]],
+|cff00ffff青蛙：|cff00ff00我感觉我还可以救一下，请将我丢进|cffff0000基地右边花园的井里|cffffff00（也可见死不救，点击左键可食用，增加10%生命上限|r）
+
+|cffcccccc抓青蛙活动物品|r]],
 --品质
 color = '紫',
 --物品类型
@@ -62,21 +63,21 @@ local function give_award(hero)
         return true
     end
     if rand_name == '无' then
-        p:sendMsg('|cffffe799【系统消息】|r 溜走了',2) 
+        p:sendMsg('|cffffe799【系统消息】|r 青蛙快乐地游走了',3) 
     elseif  finds(rand_name,'格里芬','黑暗项链','最强生物心脏','白胡子的大刀') then
         --满时，掉在地上
         hero:add_item(rand_name,true)
-        ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 兑换到了 |cffff0000'..rand_name..'|r',2) 
+        ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 救蛙一命，胜造七级浮屠，奖励 |cffff0000'..rand_name..'|r',4) 
     elseif  finds('红 金',rand_name) then   
         local list = ac.quality_item[rand_name]
         local name = list[math.random(#list)]
         --满时，掉在地上
         local item = hero:add_item(name,true)
-        p:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 兑换到了 '..item.color_name..'',2) 
+        p:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 救蛙一命，胜造七级浮屠，奖励 '..item.color_name..'',4) 
     elseif finds(rand_name,'点金石','恶魔果实','吞噬丹')  then
         --满时，掉在地上
         hero:add_item(rand_name,true)
-        ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 兑换到了 |cffff0000'..rand_name..'|r',2) 
+        ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 救蛙一命，胜造七级浮屠，奖励 |cffff0000'..rand_name..'|r',4) 
     elseif finds(rand_name,'随机技能书')  then    
         local rand_list = ac.unit_reward['商店随机技能']
         local rand_name = ac.get_reward_name(rand_list)
@@ -87,6 +88,7 @@ local function give_award(hero)
         --添加给购买者
         local name = list[math.random(#list)]
         ac.item.add_skill_item(name,hero)
+        ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 救蛙一命，胜造七级浮屠，奖励 |cffff0000'..rand_name..'|r',4) 
     elseif  rand_name == '井底之蛙' then 
         local key = ac.server.name2key(rand_name)
         if p:Map_GetServerValue(key) < 1 and p:Map_GetMapLevel()>=5 then 
@@ -96,7 +98,7 @@ local function give_award(hero)
             local skl = hero:find_skill(rand_name,nil,true) 
             if not skl  then 
                 ac.game:event_notify('技能-插入魔法书',hero,'精彩活动','井底之蛙')
-                ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r 兑换到了 |cffff0000'..rand_name..'|r',2) 
+                ac.player.self:sendMsg('|cffffe799【系统消息】|r |cff00ffff'..player:get_name()..'|r  将青蛙丢进井里，惊喜获得|cffff0000【可存档成就】'..rand_name..'|r |cff00ff00+16.8杀怪加全属性|r |cff00ff00+16.8攻击减甲|r |cff00ff00+16.8%杀敌数加成|r |cff00ff00+16.8%物理伤害加深|r',6) 
             end 
         else   
             --重新来一次
@@ -110,13 +112,13 @@ end
 ac.game:event '游戏-开始'(function()
     -- 注册材料获得事件
     local time = 60 * 5 
-    local time = 30
+    -- local time = 30
     ac.loop(time*1000,function()
         local point = ac.map.rects['藏宝区']:get_random_point()
         local unit = ac.player(16):create_unit('青蛙',point)
 
         unit:add_buff '随机逃跑' {}
-        ac.nick_name('呱呱呱!',unit,200)
+        ac.nick_name('呱呱呱',unit,150)
 
         unit:event '单位-死亡'(function()
             ac.item.create_item('奄奄一息的青蛙',unit:get_point())
--- map/script/平台/精彩活动.lua
+++ map/script/平台/精彩活动.lua
@@ -32,27 +32,27 @@ mt{
 --等级
 level = 1, --要动态插入
 --图标
-art = [[ydss.blp]],
+art = [[jdzw.blp]],
 --说明
 tip = [[
 |cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
 
 |cffffe799【获得方式】：|r
-|cff00ffff消耗 |cffff0000三十根喜鹊翎毛|r |cff00ffff兑换获得
+|cff00ffff抓青蛙活动获得
 
 |cffFFE799【成就属性】：|r
-|cff00ff00+13.8   杀怪加全属性|r
-|cff00ff00+13.8   攻击减甲|r
-|cff00ff00+13.8%  木头加成|r
-|cff00ff00+13.8%  会心伤害|r
+|cff00ff00+16.8   杀怪加全属性|r
+|cff00ff00+16.8   攻击减甲|r
+|cff00ff00+16.8%  杀敌数加成|r
+|cff00ff00+16.8%  物理伤害加深|r
 
 ]],
 --目标类型
 target_type = ac.skill.TARGET_TYPE_NONE,
-['杀怪加全属性'] = 13.8,
-['木头加成'] = 13.8,
-['攻击减甲'] = 13.8,
-['会心伤害'] = 13.8,
+['杀怪加全属性'] = 16.8,
+['木头加成'] = 16.8,
+['攻击减甲'] = 16.8,
+['会心伤害'] = 16.8,
 need_map_level = 5,
 }
 
