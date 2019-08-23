6c420aa4a55d01e75ce5b5a5560b37a3025da471
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2019-08-24 01:13:48 +0800

    Update 武林大会.lua

--- map/script/平台/武林大会.lua
+++ map/script/平台/武林大会.lua
@@ -107,27 +107,27 @@ mt{
 --等级
 level = 0,
 --图标
-art = [[wlgs.blp]],
+art = [[jsqc.blp]],
 --说明
 tip = [[|cffffff00【要求地图等级>10|cffffff00】|r
 
 |cffffe799【获得方式】：|r
-|cff00ffff比武积分|cffff0000（武林大会模式获得）|cff00ffff超过 1000  自动获得，已拥有积分：|r%wabao_cnt%
+|cff00ffff比武积分|cffff0000（武林大会模式获得）|cff00ffff超过 1500  自动获得，已拥有积分：|r%wabao_cnt%
 
 |cffFFE799【成就属性】：|r
-|cff00ff00+88 杀怪加全属性|r
-|cff00ff00+30% 物理伤害加深|r
-|cff00ff00+15% 技能伤害加深|r
-|cff00ff00+30% 暴击加深|r
-|cff00ff00+30% 技暴加深|r
+|cff00ff00+128 杀怪加全属性|r
+|cff00ff00+35% 物理伤害加深|r
+|cff00ff00+20% 技能伤害加深|r
+|cff00ff00+35% 暴击伤害加深|r
+|cff00ff00+35% 技暴伤害加深|r
  ]],
 --目标类型
 target_type = ac.skill.TARGET_TYPE_NONE,
-['杀怪加全属性'] = 88,
-['物理伤害加深'] = 30,
-['技能伤害加深'] = 15,
-['暴击加深'] = 30,
-['技暴加深'] = 30,
+['杀怪加全属性'] = 128,
+['物理伤害加深'] = 35,
+['技能伤害加深'] = 20,
+['暴击加深'] = 35,
+['技暴加深'] = 35,
 
 wabao_cnt = function(self)
     local p = ac.player.self
